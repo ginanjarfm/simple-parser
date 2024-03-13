bf6e78bb613d8a05e8672fac7a5f87ea81e19c35
ginanjar <ginanjar.fahrul.m@gmail.com>
2024-03-13 21:28:31 +0700

    example on sending data to server

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,7 +1,22 @@
 #include "event_io.h"
+#include <WiFi.h>
+
+const char* ssid = "your_wifi_ssid";
+const char* password = "your_wifi_password";
+const char* serverIP = "192.168.1.100"; // Change this to your server's IP address
+const int serverPort = 1234; // Change this to your server's port
+
+WiFiClient client;
 
 void setup() {
   Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
   insertEventIO(0x71, 0x01, 1);
   insertEventIO(0x72, 0x01, 1);
   insertEventIO(0x73, 0x0002, 2);
@@ -10,6 +25,24 @@ void setup() {
 
 void loop() {
   String hexString = toHexString();
-  Serial.println(hexString);
-  delay(1000); // Example delay
+
+  uint8_t buffer[hexString.length() / 2];
+  for (int i = 0; i < hexString.length(); i += 2) {
+    buffer[i / 2] = strtoul(hexString.substring(i, i + 2).c_str(), NULL, 16);
+  }
+
+  if (!client.connected()) {
+    if (!client.connect(serverIP, serverPort)) {
+      delay(1000);
+      return;
+    }
+  }
+
+  if (client.connected()) {
+    client.write(buffer, sizeof(buffer));
+  } else {
+    client.stop();
+  }
+
+  delay(1000);
 }
